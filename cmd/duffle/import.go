ced5028fc5d30a9141eb36f389a246e7e0013330
Ben Patterson <bentw9@gmail.com>
2019-07-24 16:18:20 +0100

    Duffle import adds to internal storage [WIP]
    Fixes https://github.com/deislabs/duffle/issues/707

--- cmd/duffle/import.go
+++ cmd/duffle/import.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
@@ -13,7 +16,18 @@ import (
 )
 
 const importDesc = `
-Unpacks a bundle from a gzipped tar file on local file system
+Imports a Cloud Native Application Bundle (CNAB) from a gzipped tar file on the local file system.
+
+This operation is symmetric with export, and so will save the bundle, along with any images 
+referenced by the bundle, to the internal storage.
+
+If --destination is specified, the file will also be unzipped to the specified path. Please
+note -d with the empty string "" will be ignored, so use '-d .' to save the unzipped bundle
+to the working directory.
+
+Example:
+	$ duffle import mybundle.tgz
+	$ duffle import mybundle.tgz -d bundles/unzipped
 `
 
 type importCmd struct {
@@ -32,7 +46,7 @@ func newImportCmd(w io.Writer) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:   "import [PATH]",
-		Short: "unpack CNAB bundle from gzipped tar file",
+		Short: "save CNAB bundle from gzipped tar file",
 		Long:  importDesc,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) == 0 {
@@ -54,12 +68,21 @@ func newImportCmd(w io.Writer) *cobra.Command {
 func (im *importCmd) run() error {
 	source, err := filepath.Abs(im.source)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error in source: %s", err)
 	}
 
-	dest, err := filepath.Abs(im.dest)
-	if err != nil {
-		return err
+	var dest string
+	if im.dest == "" {
+		dest, err = ioutil.TempDir("", "")
+		if err != nil {
+			return err
+		}
+		defer os.RemoveAll(dest)
+	} else {
+		dest, err = filepath.Abs(im.dest)
+		if err != nil {
+			return fmt.Errorf("Error in destination: %s", err)
+		}
 	}
 
 	l := loader.NewLoader()
@@ -67,5 +90,5 @@ func (im *importCmd) run() error {
 	if err != nil {
 		return err
 	}
-	return imp.Import()
+	return imp.Import(im.home)
 }
